e2481871156b5b0639a4e5fb432dba87f1bc7eac
Patrick O'Regan <patrick.oregan@pricerunner.com>
2021-08-24 09:34:14 +0200

    add position

--- src/components/PainForm/index.js
+++ src/components/PainForm/index.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-import { Box, Grid, useMediaQuery, Button, TextField } from "@material-ui/core";
-import { useTheme } from "@material-ui/core/styles";
+import {
+  Box,
+  Grid,
+  useMediaQuery,
+  Button,
+  TextField,
+  Select,
+  FormControl,
+  InputLabel,
+} from "@material-ui/core";
+import { useTheme, makeStyles } from "@material-ui/core/styles";
 import useLogs from "../../utils/useLogs";
 import Slider from "../Slider";
+import usePositions from "../../utils/usePositions";
 
 const date = new Date();
 date.setHours(date.getHours() + 2);
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    minWidth: "100%",
+  },
+}));
 
 const getDefaultLog = () => ({
   upperBack: 0,
@@ -13,6 +29,7 @@ const getDefaultLog = () => ({
   lowerBack: 0,
   glutes: 0,
   mind: 0,
+  position: "",
   time: date.toISOString().slice(0, 16),
 });
 
@@ -22,9 +39,12 @@ const PainForm = () => {
     defaultMatches: true,
   });
 
+  const classes = useStyles();
+
   const [log, setLog] = useState(getDefaultLog());
 
   const { addLog } = useLogs();
+  const { positions, addPosition } = usePositions();
 
   return (
     <Box p={4} width={"2/3"}>
@@ -67,6 +87,57 @@ const PainForm = () => {
             value={log.mind}
             setValue={(mind) => setLog({ ...log, mind })}
           />
+        </Grid>
+
+        <Grid item xs={12}>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="simple-select-label">Position</InputLabel>
+            <Select
+              native
+              value={log.position}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setLog({ ...log, position: event.target.value });
+              }}
+              inputProps={{
+                name: "position",
+                id: "position-native-simple",
+              }}
+            >
+              {!!positions &&
+                positions.map(({ id, position }) => (
+                  <option key={id} value={position}>
+                    {position}
+                  </option>
+                ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="position"
+            label="Position"
+            type="text"
+            defaultValue={""}
+            onChange={(event) => {
+              console.log({ onChange: event.target.value });
+              setLog({ ...log, position: event.target.value });
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Box m={1}>
+            <Button
+              onClick={() => {
+                addPosition({ position: log.position });
+              }}
+              variant="contained"
+              color="primary"
+            >
+              Add position
+            </Button>
+          </Box>
         </Grid>
         <Grid item xs={12}>
           <TextField
